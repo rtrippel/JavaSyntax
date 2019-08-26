3b46fb62b949f4c479767b5174ae43095a6520c9
REM <>
2019-08-26 12:47:40 +0300

    add Volatile

---
+++ src/main/java/Multithreading/Volatile.java
@@ -0,0 +1,25 @@
+package Multithreading;
+
+public class Volatile {
+
+    private String text = "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!END!!!!!!!!!!!!!!!!!!!!!!!";
+
+    public Volatile() {
+        new Thread(){
+            volatile boolean flag = false; //ключевое слово volatile не дает кешироваться переменной.
+            // Т.е. говорит о том что переменная изменяемая, может измениться, поэтому процессор ее постоянно чекает.
+            private int i;
+            @Override
+            public void run() {
+                while (!flag){
+                    i++;
+
+                    if (i >100000) {
+                        flag = true;
+                        System.out.println(text);
+                    }
+                }
+            }
+        }.start();
+    }
+}
---
+++ src/test/java/Multithreading/VolatileTest.java
@@ -0,0 +1,42 @@
+package Multithreading;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.io.*;
+import java.lang.reflect.Field;
+
+public class VolatileTest {
+    private PrintStream consoleStream;
+    private Volatile v;
+    private ByteArrayOutputStream outputStream;
+    private PrintStream stream;
+
+    @Before
+    public void createAndOutSave() {
+        consoleStream = System.out;
+        v = new Volatile();
+        outputStream = new ByteArrayOutputStream();
+        stream = new PrintStream(outputStream);
+    }
+
+    @Test
+    public void threadShouldBeEnd () throws InterruptedException, IllegalAccessException {
+
+
+        System.setOut(stream); // перенаправили поток вывода в консоль в буффер
+
+        Thread.sleep(1000); // ждем когда отработает поток созданный при сознаии объекта Volatile
+        String result = outputStream.toString(); //записали поток в строку
+
+        System.setOut(consoleStream); // вернули поток в консоль
+        Field field = Volatile.class.getDeclaredFields()[0]; //используя рефлексию  получили список полей
+        field.setAccessible(true); // получаем доступ к приватным полям
+        // если переменная закешируется, то поток(нить) не завершится и не выведет в консоль сообщение.
+        Assert.assertEquals(result.trim(), field.get(v)); //сравниваем поток консоли с приватным полем Volatile
+        //trim  используем т.к. присутсвует перенос строки
+
+    }
+}
