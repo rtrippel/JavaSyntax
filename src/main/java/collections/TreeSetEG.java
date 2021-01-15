edcfb07df45c150ae87b59d97936ad01c228764b
Roman Trippel <rtrippel84@gmail.com>
2021-01-16 00:14:36 +0300

    Рефакторинг.

--- src/main/java/collections/TreeSetEG.java
+++ src/main/java/collections/TreeSetEG.java
@@ -55,13 +55,6 @@ public class TreeSetEG {
             this.age = age;
         }
 
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-
         @Override
         public String toString() {
             return "Cat{" +
